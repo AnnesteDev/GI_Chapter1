622378d49b7531badd404e9b9698fa9ad80e1c29
Steven Grobler <steven@anneste.com>
2014-09-15 16:27:49 +0200

    Program 1.8

--- Win32Project1/print_string.cpp
+++ Win32Project1/print_string.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+#define PRINTHELLO cout << "Hello" << endl;
+
 int main()
 {
 	/*string firstname = "";
@@ -46,21 +48,34 @@ int main()
 	//cout << "sizeof(float)	= " << sizeof(float) << endl;
 	//cout << "sizeof(double)  = " << sizeof(double) << endl;
 
-	// Demonstrates some type conversion
-	// Case 1: Convert from a less precise type to a more precise type
-	char c = 10;
-	short s = c;
-	cout << "char to short: " << s << endl;
-	
-	// Case 2: Convert from a more precise integer to a less precise integer
-	unsigned char uc = 260;
-	cout << "int to uchar: " << (int)uc << endl;
+	//// Demonstrates some type conversion
+	//// Case 1: Convert from a less precise type to a more precise type
+	//char c = 10;
+	//short s = c;
+	//cout << "char to short: " << s << endl;
+	//
+	//// Case 2: Convert from a more precise integer to a less precise integer
+	//unsigned char uc = 260;
+	//cout << "int to uchar: " << (int)uc << endl;
 
-	// Case 3: Convert from a float to an int, assuming the int can store the float's value
-	int i = 496512.5456f;
-	cout << "float to int: " << i << endl;
+	//// Case 3: Convert from a float to an int, assuming the int can store the float's value
+	//int i = 496512.5456f;
+	//cout << "float to int: " << i << endl;
 
-	// Case 4: Convert from a float to a short, this time the int can't store the float
-	s = 496512.987123f;
-	cout << "float to short: " << s << endl;
+	//// Case 4: Convert from a float to a short, this time the int can't store the float
+	//s = 496512.987123f;
+	//cout << "float to short: " << s << endl;
+
+	//PRINTHELLO
+	//PRINTHELLO
+	//PRINTHELLO
+	//PRINTHELLO
+
+	int k = 7;
+	cout << k << endl;
+
+	cout << "++k = " << ++k << endl;
+	cout << "k++ = " << k++ << endl;
+
+	cout << k << endl;
 }
