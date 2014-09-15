e2cbf4380445355404faa63e636c474ccc34939a
Steven Grobler <steven@anneste.com>
2014-09-15 15:54:30 +0200

    Program 1.4

--- Win32Project1/print_string.cpp
+++ Win32Project1/print_string.cpp
@@ -10,9 +10,30 @@ using namespace std;
 
 int main()
 {
-	string firstname = "";
+	/*string firstname = "";
 	cout << "Enter your first name and press Enter: ";
 	cin >> firstname;
 	cout << std::endl;
-	cout << "Hello, " << firstname << std::endl << std::endl;
+	cout << "Hello, " << firstname << std::endl << std::endl;*/
+
+	// Declare variables and define them to some default value
+	char letter = 'A';
+	int integer = 0;
+	float dec = 0.0f;
+
+	cout << "Enter a letter: ";
+	cin >> letter;
+
+	cout << "Enter an integer: ";
+	cin >> integer;
+
+	cout << "Enter a float number: ";
+	cin >> dec;
+
+	cout << endl;
+
+	// Now output back the values the user entered
+	cout << "Letter: " << letter << endl;
+	cout << "Integer: " << integer << endl;
+	cout << "Float: " << dec<< endl;
 }
